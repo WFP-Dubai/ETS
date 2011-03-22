00f53aa05c4f2ad535f6927c75bd1290843cac71
tobcar <tobcar@99c06ef4-7786-0410-a010-fa23ed5322c1>
2011-03-22 08:07:19 +0000

    remove ofliner and config

--- config/tobymb.py
+++
@@ -1,7 +0,0 @@
-from datetime import *
-# Django settings for ets project.
-from  settings_default import *
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-DATABASES['default'] = ALL_DB['default_tc_mb']
-DATABASES['compas'] = ALL_DB['compas_test_pal']
