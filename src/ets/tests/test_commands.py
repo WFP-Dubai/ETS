28e63a01fceb94578acff6226ac1c8dfa5602568
Arvid Paeglit <arvid@arpaso.com>
2011-08-08 10:58:02 +0400

    dispatch and receipt points become one model Warehouse

---
+++ src/ets/tests/test_commands.py
@@ -0,0 +1,25 @@
+### -*- coding: utf-8 -*- ####################################################
+
+import datetime
+from functools import wraps
+
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+from django.test import TestCase
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.http import HttpResponseNotAllowed
+from django.utils.datastructures import MultiValueDictKeyError 
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.management import call_command
+
+import ets.models
+
+class CommandTestCase(TestCase):
+    
+    #multi_db = True
+    fixtures = ['compas.json', ]
+    
+    def test_sync_compas(self):
+        call_command('sync_compas', nodelete=True)
+            
