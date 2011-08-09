b7938b92939ee4e602432d3768c2cd5602d7c073
predatell <seryi_belyi@inbox.ru>
2011-08-09 13:04:40 +0400

    Comments

--- src/ets/tests/test_warehouse.py
+++ src/ets/tests/test_warehouse.py
@@ -11,13 +11,16 @@ class WarehouseTestCase(TestCase):
     
     def test_get_warehouses(self):
         """Check method get_warehouses for model Warehouse"""
-        call_command('sync_compas', nodelete=True)
-        location = Location.objects.get(pk="OE7X")
+        # Synchronizes data with compas database
+        call_command('sync_compas', nodelete=True) 
+        location = Location.objects.get(pk="OE7X") # Create location with pk OE7X
         warehouses = Warehouse.get_warehouses(location=location)
-        self.assertEqual(warehouses.count(),1)
+        self.assertEqual(warehouses.count(),1) # Count of queryset should be 1 
+        # Check for equal results of get_warehouses for field name 
         self.assertEqual(warehouses[0].name, "EDO OFFICE MEHTI")
+        # Another check 
         location = Location.objects.get(code="ISBX")
         ogranization = Consignee.objects.get(code="DOEAF")
         warehouses = Warehouse.get_warehouses(location=location, organization=ogranization)
-        self.assertEqual(warehouses.count(),2)
+        self.assertEqual(warehouses.count(),2) # Count of queryset should be 2
     
