ab871f947a1c14bebd8df6114adf44514c0cd857
predatell <seryi_belyi@inbox.ru>
2011-08-15 13:55:34 +0400

    Change in urls with max width for line

--- src/ets/api/urls.py
+++ src/ets/api/urls.py
@@ -29,29 +29,40 @@ receiving_waybill_resource = Resource(ReceivingWaybillHandler)
 #history_id = Resource(HistoryIdHandler, authentication=AUTHENTICATORS)
 #history_date = Resource(HistoryDateHandler, authentication=AUTHENTICATORS)
 #history_user = Resource(HistoryUserHandler, authentication=AUTHENTICATORS)
-
+format_csv = {'emitter_format': 'csv'}
 
 urlpatterns = patterns('',
                        
     # For Waybills CSV API
-    (r'^waybills/warehouse_destination_waybill/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/(?P<slug>[-\w]+)/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"),
-    (r'^waybills/warehouse_destination/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"),
-    (r'^waybills/warehouse_waybill/(?P<warehouse>[-\w]+)/(?P<slug>[-\w]+)/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"),
-    (r'^waybills/destination_waybill/(?P<destination>[-\w]+)/(?P<slug>[-\w]+)/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"), 
-    (r'^waybills/warehouse/(?P<warehouse>[-\w]+)/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"),
-    (r'^waybills/destination/(?P<destination>[-\w]+)/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"),                 
-    (r'^waybills/(?P<slug>[-\w]+)/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"),
-    (r'^waybills/$', waybills_resource, { 'emitter_format': 'csv' }, "api_waybills"),
+    (r'^waybills/warehouse_destination_waybill/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/(?P<slug>[-\w]+)/$', 
+        waybills_resource, format_csv, "api_waybills"),
+    (r'^waybills/warehouse_destination/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/$', waybills_resource, 
+        format_csv, "api_waybills"),
+    (r'^waybills/warehouse_waybill/(?P<warehouse>[-\w]+)/(?P<slug>[-\w]+)/$', waybills_resource, 
+        format_csv, "api_waybills"),
+    (r'^waybills/destination_waybill/(?P<destination>[-\w]+)/(?P<slug>[-\w]+)/$', waybills_resource, 
+        format_csv, "api_waybills"), 
+    (r'^waybills/warehouse/(?P<warehouse>[-\w]+)/$', waybills_resource, format_csv, "api_waybills"),
+    (r'^waybills/destination/(?P<destination>[-\w]+)/$', waybills_resource, format_csv, "api_waybills"),                 
+    (r'^waybills/(?P<slug>[-\w]+)/$', waybills_resource, format_csv, "api_waybills"),
+    (r'^waybills/$', waybills_resource, format_csv, "api_waybills"),
     
     # For LoadingDetails CSV API
-    (r'^loading_details/warehouse_destination_waybill/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/(?P<waybill>[-\w]+)/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"),
-    (r'^loading_details/warehouse_destination/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"),
-    (r'^loading_details/warehouse_waybill/(?P<warehouse>[-\w]+)/(?P<waybill>[-\w]+)/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"),
-    (r'^loading_details/destination_waybill/(?P<destination>[-\w]+)/(?P<waybill>[-\w]+)/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"), 
-    (r'^loading_details/warehouse/(?P<warehouse>[-\w]+)/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"),
-    (r'^loading_details/destination/(?P<destination>[-\w]+)/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"),                 
-    (r'^loading_details/(?P<waybill>[-\w]+)/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"),
-    (r'^loading_details/$', loading_details_resource, { 'emitter_format': 'csv' }, "api_loading_details"),
+    (r'^loading_details/warehouse_destination_waybill/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/(?P<waybill>[-\w]+)/$', 
+        loading_details_resource, format_csv, "api_loading_details"),
+    (r'^loading_details/warehouse_destination/(?P<warehouse>[-\w]+)/(?P<destination>[-\w]+)/$', 
+        loading_details_resource, format_csv, "api_loading_details"),
+    (r'^loading_details/warehouse_waybill/(?P<warehouse>[-\w]+)/(?P<waybill>[-\w]+)/$', loading_details_resource, 
+        format_csv, "api_loading_details"),
+    (r'^loading_details/destination_waybill/(?P<destination>[-\w]+)/(?P<waybill>[-\w]+)/$', loading_details_resource, 
+        format_csv, "api_loading_details"), 
+    (r'^loading_details/warehouse/(?P<warehouse>[-\w]+)/$', loading_details_resource, format_csv, 
+        "api_loading_details"),
+    (r'^loading_details/destination/(?P<destination>[-\w]+)/$', loading_details_resource, format_csv, 
+        "api_loading_details"),                 
+    (r'^loading_details/(?P<waybill>[-\w]+)/$', loading_details_resource, format_csv, 
+        "api_loading_details"),
+    (r'^loading_details/$', loading_details_resource, format_csv, "api_loading_details"),
     
     (r'^new/$', new_waybill_resource, { 'emitter_format': 'django_json' }, "api_new_waybill"),
     (r'^receiving/(?P<destination>[-\w]+)/$', receiving_waybill_resource, { 
