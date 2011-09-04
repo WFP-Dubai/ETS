f79dd2a28fa4090ff8bf32255e5ede116ad98936
Arvid Paeglit <arvid@arpaso.com>
2011-09-04 17:05:30 +0400

    reception condition in tag. Waybill must be signed

--- src/ets/templatetags/extra_tags.py
+++ src/ets/templatetags/extra_tags.py
@@ -57,7 +57,8 @@ def waybill_reception(waybill, user, text=_("Recept")):
     return { 
             'text': text,
             'url': reverse('waybill_reception', kwargs={'waybill_pk': waybill.pk}),
-            'success': not (waybill.get_receipt() and waybill.get_receipt().signed_date) 
+            'success': waybill.transport_dispach_signed_date is None \
+                and not (waybill.get_receipt() and waybill.get_receipt().signed_date) 
                 and Warehouse.filter_by_user(user).filter(pk=waybill.destination.pk).count(),
     }
 
