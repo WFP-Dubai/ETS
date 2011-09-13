545aaa9b8fb77539961a2761c43d9e8845cb9bf4
Arvid Paeglit <arvid@arpaso.com>
2011-09-13 15:53:54 +0400

    WarehouseAdmin fixes

--- src/ets/admin.py
+++ src/ets/admin.py
@@ -180,7 +180,7 @@ class WarehouseAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'organization', 'location', 'compas', 'start_date',)
     list_filter = ('location', 'start_date')
     date_hierarchy = 'start_date'
-    search_fields = ('pk', 'title', 'place__name',)
+    search_fields = ('pk', 'name', 'location__name', 'organization__name', 'compas__code')
     inlines = (StockInline, OrderInline)
     
 admin.site.register( ets.models.Warehouse, WarehouseAdmin )
