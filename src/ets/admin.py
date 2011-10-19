0bfe36dd33d4b2b31e361b8cab0e26260cd788b0
Arvid Paeglit <arvid@arpaso.com>
2011-10-19 14:06:41 +0400

    dummy

--- src/ets/admin.py
+++ src/ets/admin.py
@@ -163,7 +163,7 @@ class PersonInline(admin.TabularInline):
 
 class WarehouseAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'organization', 'location', 'compas', 'start_date')
-    list_filter = ('location', 'start_date')
+    list_filter = ('start_date', 'location', )
     search_fields = ('code', 'name', 'location__name', 'organization__name', 'compas__code')
     inlines = (StockInline,)
     
