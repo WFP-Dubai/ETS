c03b832fe769853b343a9e660b8aa99f704aa1cf
Arvid Paeglit <arvid@arpaso.com>
2012-01-26 15:15:51 +0400

    removed extra filters on LTIOriginal duting import to ETS

--- src/ets/utils.py
+++ src/ets/utils.py
@@ -177,9 +177,10 @@ def import_order(compas):
         places = _get_places(compas)
         for lti in compas_models.LtiOriginal.objects.using(compas)\
                             .filter(models.Q(expiry_date__gte=today) | models.Q(expiry_date__isnull=True),
-                                    consegnee_code__in=places.values_list('organization_id', flat=True),
+                                    #consegnee_code__in=places.values_list('organization_id', flat=True),
                                     origin_wh_code__in=places.values_list('org_code', flat=True),
-                                    destination_location_code__in=places.values_list('geo_point_code', flat=True)):
+                                    #destination_location_code__in=places.values_list('geo_point_code', flat=True)
+                                    ):
             
             #Create Order
             defaults = {
