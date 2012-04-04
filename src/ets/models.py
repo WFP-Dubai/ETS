0f8540cc68a1cef8af1be3b01c9b463d190d77f4
Tobias Carlander <tobias.carlander@wfp.org>
2012-04-04 14:46:52 +0400

    Filter fix

--- src/ets/models.py
+++ src/ets/models.py
@@ -384,8 +384,10 @@ class Order(models.Model):
         """Retrieves stock items for current order through warehouse"""
         
         return StockItem.objects.filter(pk__in=chain(*(
-                                            item.get_stock_items().values_list('pk', flat=True) for item in self.items.all()))).filter(quantity_net!=0)
+                                            item.get_stock_items().values_list('pk', flat=True) for item in self.items.all()))))
         
+        
+    
         #=======================================================================
         # return StockItem.objects.filter(warehouse__orders=self,
         #                                project_number=F('warehouse__orders__items__project_number'),
