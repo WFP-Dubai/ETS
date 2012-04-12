494fbf913a7f6ba504c64a98f0c2bee51f35be08
Tobias Carlander <tobias.carlander@wfp.org>
2012-04-12 19:50:51 +0400

    Fix Position Remarks Field

--- src/ets/forms.py
+++ src/ets/forms.py
@@ -165,7 +165,7 @@ class WaybillRecieptForm( forms.ModelForm ):
     
     # create the layout object
     helper.add_layout(Layout(
-        Fieldset(ugettext('Destination'), Row('destination', 'distance'), 'receipt_remarks'),
+        Fieldset('receipt_remarks',ugettext('Destination'), Row('destination', 'distance')),
         Fieldset(ugettext('Dates'), Row('arrival_date', 'start_discharge_date', 'end_discharge_date')),
         Fieldset(ugettext('Containers'), 
                  Row('container_one_remarks_reciept', 'container_two_remarks_reciept'),),
